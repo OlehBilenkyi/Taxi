ca8181b9d9a03f54ef43862635c4769a54134fdd
OlehBilenkyi <I3eLuy@gmail.com>
2025-03-16 00:40:40 +0100

    1

--- admin_panel/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/admin/", // <-- ВАЖНО! Если админка размещена в поддиректории
-  plugins: [react()],
-});
